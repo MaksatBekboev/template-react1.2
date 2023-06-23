60954760ab7c1806dab8bbbd7fb9219fa9438205
MaksatBekboev <132592881+MaksatBekboev@users.noreply.github.com>
2023-06-23 20:32:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [isSwapped, setIsSwapped] = useState(false);
+function RegistrationForm() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
 
-  const swapColors = () => {
-    setIsSwapped(!isSwapped);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const message = `ФИО: ${name}\nНомер телефона: ${phone}\nEmail адрес: ${email}`;
+    alert(message);
+
+    console.log("ФИО:", name);
+    console.log("Номер телефона:", phone);
+    console.log("Email адрес:", email);
+
+    setName("");
+    setPhone("");
+    setEmail("");
   };
 
   return (
-    <div style={{ display: "flex" }}>
-      <div
-        style={{
-          width: "300px",
-          height: "300px",
-          backgroundColor: isSwapped ? "blue" : "red",
-        }}
-        onClick={swapColors}
-      />
-      <div
-        style={{
-          width: "300px",
-          height: "300px",
-          backgroundColor: isSwapped ? "red": "blue",
-        }}
-        onClick={swapColors}
-      />
+    <div>
+      <h1>Форма регистрации пользователя</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">ФИО:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+
+        <label htmlFor="phone">Номер телефона:</label>
+        <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+
+        <label htmlFor="email">Email адрес:</label>
+        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+        <button type="submit">Отправить</button>
+      </form>
     </div>
   );
-};
+}
 
-export default App;
+export default RegistrationForm;
